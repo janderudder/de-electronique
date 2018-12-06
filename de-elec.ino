8d9f05265dca1cbd8bdc1ecaa306298a52775141
janderudder <39810873+janderudder@users.noreply.github.com>
2018-12-06 11:38:37 +0100

    cleaned main file.

--- de-elec.ino
+++ de-elec.ino
@@ -1,28 +1,20 @@
 /**
- * Un dé hexadécimal électronique avec
- * un afficheur 7 segments.
+ * Un dé hexadécimal électronique 
+ * tirant des nombres de 0 à F,
+ * avec un afficheur 7 segments.
  */
-#include "DisplayModifier.hpp"
 #include "SevenSegmentDisplay.hpp"
 #include "Clock.hpp"
 
 using D7 = SevenSegmentDisplay;
-using ulong = unsigned long;
 using time_t = decltype(millis());
 
+// Brochage
 const int ledPin = 10;    // Broche permettant la fonction lancé de dé
 const int swPin = 11;     // Broche lisant l'appui sur le bouton
 
-// Etat de la broche
-int ledPinState = HIGH;
-
-
 // Afficheur 7 segments
 D7 d7(D7::COMCATHODE);
-
-// Modifier
-DisplayModifier d7Mod(d7);
-
 
 
 void setup()
@@ -41,48 +33,28 @@ void setup()
   d7.initPin(D7::Seg_F, 5);
   d7.initPin(D7::Seg_G, 3);
   d7.initPin(D7::Seg_P, 9);
-
-  // Initialisation d'un affichage éteint
-  d7.clear();
-  //d7.display(D7::Predef::Val_B);
-
-  // Initialisation de la broche de jeu
-  digitalWrite(ledPin, ledPinState);
-
-  //Serial.begin(9600);
-  //delay(1800);
   
+  // Initialisation de l'aléatoire  
   randomSeed(analogRead(0));
 }
 
 
-// Clock
-Clock clock;
-time_t timer = 0;
 
 // Random
-int randomNumber;
+int randomNumber = 0;
 
 
 void loop()
 {
-  auto loopTime = clock.update(millis()).getTime();
-
   if (digitalRead(swPin) == HIGH) {
-    ledPinState = LOW;
-    digitalWrite(ledPin, ledPinState);
-    timer = 0;
-    randomNumber = random(0, 16);
+    digitalWrite(ledPin, LOW);
+    randomNumber = random(0xF + 1);
   }
   
   else {
-    timer += loopTime;
     d7.display(randomNumber);
     if (digitalRead(swPin) == LOW) {
-      ledPinState = HIGH;
-      digitalWrite(ledPin, ledPinState);
+      digitalWrite(ledPin, HIGH);
     }
   }
-  
-  delay(16);
 }
